b2ec2b6cd5a6df1e8ed53a9743f859c53a24da29
JakubBankowski <s33203@pjwstk.edu.pl>
2026-06-03 19:18:57 +0200

    Finished state controller

--- APBD_10/Services/StateContainer.cs
+++ APBD_10/Services/StateContainer.cs
@@ -1,6 +1,35 @@
-﻿namespace APBD_10.Services;
+﻿using APBD_10.Models;
+
+namespace APBD_10.Services;
 
 public class StateContainer
 {
+    private readonly List<StudentDto> _students = new();
+
+    private IReadOnlyList<StudentDto> ObservedStudents => _students.AsReadOnly();
+
+    public event Action? OnChange;
+
+    public void AddObserved(StudentDto student)
+    {
+        if (!_students.Any(x => x.Id == student.Id))
+        {
+            _students.Add(student);
+            NotifyChange();
+        }
+    }
+
+    public void RemoveObserved(int studentId)
+    {
+        var student = _students.FirstOrDefault(x => x.Id == studentId);
+        if (student != null)
+        {
+            _students.Remove(student);
+            NotifyChange();
+        }
+    }
     
+    public bool IsObserved(int studentId) => _students.Any(x => x.Id == studentId);
+    
+    void NotifyChange() => OnChange?.Invoke();
 }
